c8deb6db34266d30342b7f799d1cff8ae8bc5fd9
zakora <zakora@users.noreply.github.com>
2018-10-06 16:10:51 +0200

    release version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='borgback',
-    version='0.1',
+    version='0.2',
     description='Scheduled backups using Borg',
     author='zakora',
     classifiers=[
